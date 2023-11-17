ad746eb44b42d07bcd77cef7685be9909a386d05
Jonathan Yu <yu.jona@northeastern.edu>
2023-11-17 17:42:52 -0500

    Finished Hw except for adding render.com account

--- Courses/index.js
+++
@@ -1,8 +0,0 @@
-import Database from "../Database/index.js";
-function CourseRoutes(app) {
-  app.get("/api/courses", (req, res) => {
-    const courses = Database.courses;
-    res.send(courses);
-  });
-}
-export default CourseRoutes;
