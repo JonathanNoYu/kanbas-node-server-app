9d44eab4b7f3af833e30e73c6227df5e17362752
Jonathan Yu <yu.jona@northeastern.edu>
2023-12-04 00:16:34 -0500

    Finished up until 4

--- app.js
+++ app.js
@@ -5,12 +5,17 @@ import cors from "cors";
 import CourseRoutes from "./Courses/routes.js";
 import ModuleRoutes from "./Modules/routes.js";
 import "dotenv/config";
+import mongoose from "mongoose";
+import UserRoutes from "./Users/routes.js";
 
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
 Hello(app);
+UserRoutes(app);
 app.listen(process.env.PORT || 4000);
